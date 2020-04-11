ce2c9b9c1157d44c15e8a0a07f6becb79def84da
zoranzoki21 <zorandori4444@gmail.com>
2020-04-11 17:38:32 +0200

    Add alias for Serbian
    Change-Id: I0d5aacff2e1392be2bd61de77419ea67bebd8685

--- PrivateDomains.alias.php
+++ PrivateDomains.alias.php
@@ -12,3 +12,13 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'PrivateDomains' => [ 'PrivateDomains' ],
 ];
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = [
+	'PrivateDomains' => [ 'ПриватниДомени' ],
+];
+
+/** Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = [
+	'PrivateDomains' => [ 'PrivatniDomeni' ],
+];
